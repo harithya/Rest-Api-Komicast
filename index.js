575b919b1b1f04d3be5ea0fdd0cf654fddf507a7
Harithya Wisesa <harithya77@gmail.com>
2024-05-09 19:01:11 +0700

    add dotenv

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+require("dotenv").config();
+
+const {
+  env: { PORT, BASE_URL },
+} = process;
 
 const homeRoute = require("./src/app/komik/komik.route");
 const masterRouter = require("./src/app/master/master.route");
 app.use("/api/", homeRoute);
 app.use("/api/", masterRouter);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${port}, http://localhost:${port}`);
 });
