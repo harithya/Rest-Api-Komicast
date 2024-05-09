68fd91f00ed9a395acc08db9cc30481b9a7801e4
Harithya Wisesa <harithya77@gmail.com>
2024-05-09 19:03:34 +0700

    change port

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
-const {
-  env: { PORT, BASE_URL },
-} = process;
+const port = process.env.PORT || 3000;
 
 const homeRoute = require("./src/app/komik/komik.route");
 const masterRouter = require("./src/app/master/master.route");
 app.use("/api/", homeRoute);
 app.use("/api/", masterRouter);
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}, http://localhost:${port}`);
 });
