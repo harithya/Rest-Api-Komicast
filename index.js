4a506955db1de2da0898bbedbe9c7bd13cebbaa4
Harithya Wisesa <harithya77@gmail.com>
2024-05-09 19:24:46 +0700

    add endpoint root

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const homeRoute = require("./src/app/komik/komik.route");
 const masterRouter = require("./src/app/master/master.route");
 app.use("/api/", homeRoute);
 app.use("/api/", masterRouter);
+app.get("/", function (req, res) {
+  return res.send("Hello World");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}, http://localhost:${port}`);
